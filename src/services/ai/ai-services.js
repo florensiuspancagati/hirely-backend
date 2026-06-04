cbc483d3a076b7fe32de6b59ff1e3b9c31336ec5
florensiuspancagati <florensiuspancagati@gmail.com>
2026-06-04 14:47:19 +0700

    Integrasi FE BE AI done

--- src/services/ai/ai-services.js
+++ src/services/ai/ai-services.js
@@ -1,37 +1,67 @@
-/* eslint-disable no-unused-vars */
-const aiForAnalysesCV = async ({ extractedFile, jobDescription }) => {
-  // sementara mock dulu
-  // console.log(extractedFile);
-  // console.log(jobDescription);
+// /* eslint-disable no-unused-vars */
+// const aiForAnalysesCV = async ({ cv_text, jobDescription }) => {
+//   // sementara mock dulu
+//   // console.log(cv_text);
+//   // console.log(jobDescription);
 
-  return {
-    score: 78,
-    summary: 'CV cukup sesuai untuk posisi Backend Developer namun masih kurang pada deployment dan testing.',
+//   return {
+//     score: 78,
+//     summary: 'CV cukup sesuai untuk posisi Backend Developer namun masih kurang pada deployment dan testing.',
     
-    matchedSkills: [
-      'Node.js',
-      'Express.js',
-      'MongoDB',
-    ],
+//     matchedSkills: [
+//       'Node.js',
+//       'Express.js',
+//       'MongoDB',
+//     ],
 
-    missingSkills: [
-      'Docker',
-      'CI/CD',
-      'Unit Testing',
-    ],
+//     missingSkills: [
+//       'Docker',
+//       'CI/CD',
+//       'Unit Testing',
+//     ],
 
-    improvements: [
-      'Tambahkan project deployment',
-      'Tambahkan testing pada project',
-      'Perjelas deskripsi pengalaman',
-    ],
+//     improvements: [
+//       'Tambahkan project deployment',
+//       'Tambahkan testing pada project',
+//       'Perjelas deskripsi pengalaman',
+//     ],
 
-    recommendedSkills: [
-      'Docker',
-      'GitHub Actions',
-      'Jest',
-    ],
-  };
+//     recommendedSkills: [
+//       'Docker',
+//       'GitHub Actions',
+//       'Jest',
+//     ],
+//   };
+// };
+
+// export default aiForAnalysesCV;
+
+const aiForAnalysesCV = async ({ cv_text, jobDescription }) => {
+  const response = await fetch(
+    process.env.FASTAPI_AI_URL,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        cv_text: cv_text,
+        job_description: jobDescription,
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    const errorBody = await response.text();
+
+    throw new Error(
+      `AI Service Error (${response.status}): ${errorBody}`
+    );
+  }
+
+  const result = await response.json();
+
+  return result.data;
 };
 
 export default aiForAnalysesCV;
